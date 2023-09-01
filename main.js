772d8776f20f11779bb7522b75c52bd7caf5794d
eden <1denchen.work@gmail.com>
2023-09-02 00:24:49 +0800

    [feat] index.html

--- main.js
+++ main.js
@@ -1,4 +1,41 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
-console.log("Hello world!");
+const carouselSwiper = new Swiper('.carousel-swiper', {
+    autoplay: {
+        delay: 3000,
+    },
+
+    direction: 'horizontal',
+    loop: false,
+
+    navigation: {
+        nextEl: '.button-next',
+        prevEl: '.button-prev',
+    },
+});
+
+//環形文字
+const text = document.querySelector(".more-txt");
+let length = text.innerText.length;
+let degree = 360 / length;
+text.innerHTML = text.innerText
+    .split("")
+    .map(
+        (char, i) => `<span style="transform:rotate(${i * degree}deg)">${char}</span>`
+    )
+    .join("");
+
+//使用分享
+const commentSwiper = new Swiper('.comment-swiper', {
+    // slide 間距
+    spaceBetween: 24,
+    slidesPerView: 3,
+    // Scrollbar
+    scrollbar: {
+        el: ".swiper-scrollbar",
+        draggable: true
+    },
+    // 使 swiper 可以使用滑鼠滾輪捲動
+    mousewheel: true,
+});
